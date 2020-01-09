21be65e2827682d47dfd58985aebf641d6f35fa7
Connor Claxton <czclaxton@gmail.com>
2020-01-08 21:01:00 -0600

    finished up until more functionality and add redux store

---
+++ adventuregame/frontend/src/components/layout/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Display from "./Display/Display";
+
+export default function LandingPage() {
+  return (
+    <>
+      <h1>LANDINGPAGE</h1>
+      <Display />
+    </>
+  );
+}
